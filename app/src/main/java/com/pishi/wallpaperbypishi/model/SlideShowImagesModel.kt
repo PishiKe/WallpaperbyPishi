248f3cdd2d8d5ece036fe61934869aabc889b94f
PishiKe <nashpeter33@gmail.com>
2022-03-10 00:44:02 +0300

    Update

--- app/src/main/java/com/pishi/wallpaperbypishi/model/SlideShowImagesModel.kt
+++ app/src/main/java/com/pishi/wallpaperbypishi/model/SlideShowImagesModel.kt
@@ -5,33 +5,39 @@ import com.pishi.wallpaperbypishi.R
 data class SlideShowImagesModel(
     val title : String,
     val caption : String,
-    val imageUrl : Int
+    val imageUri : Int,
+    val rating : Float
 )
 
 val slidePhotos = listOf(
     SlideShowImagesModel(
         "photo1",
         "Caption1",
-    R.drawable.bird_at_mwaani
+    R.drawable.bird_at_mwaani,
+        4.0f
     ),
     SlideShowImagesModel(
         "photo2",
         "Caption2",
-        R.drawable.birds
+        R.drawable.birds,
+        4.0f
     ),
     SlideShowImagesModel(
         "photo3",
         "Caption3",
-        R.drawable.birds2
+        R.drawable.birds2,
+        4.0f
     ),
     SlideShowImagesModel(
         "photo4",
         "Caption4",
-        R.drawable.birds_tree
+        R.drawable.birds_tree,
+        4.0f
     ),
     SlideShowImagesModel(
         "photo5",
         "Caption5",
-        R.drawable.britam_tower
+        R.drawable.britam_tower,
+        4.0f
     )
 )
